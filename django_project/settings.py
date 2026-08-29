0650ed9920771766cdb6f2709eeacdc9a2e525f9
tany <tanyamoh001@gmail.com>
2026-08-30 00:57:44 +0330

    replace sqlite with postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,16 +80,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# DATABASES = {
-#     "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
